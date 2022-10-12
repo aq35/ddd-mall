34a31dc9fdae253cee1197a0807f1792bd2fa121
kazuma hosobe <32251518+aq35@users.noreply.github.com>
2022-10-12 23:37:45 +0900

    Update AsyncEventEmitterTest.php

--- backend/DesignPattern/Tests/AsyncEventEmitterTest.php
+++ backend/DesignPattern/Tests/AsyncEventEmitterTest.php
@@ -24,34 +24,34 @@ use DesignPattern\Event\Domain\EventEmitterMode;
 final class AsyncEventEmitterTest extends TestCase
 {
 
-    public function testEventEmitter_async_emitters(): void
-    {
-        $loop = new Loop(new SelectLoop());
+    // public function testEventEmitter_async_emitters(): void
+    // {
+    //     $loop = new Loop(new SelectLoop());
 
-        $baseEmitter  = new EventEmitter(); // this is basic, synchronous EventEmitter
-        $asyncEmitter = new EventEmitter($loop); // this EventEmitter uses loop to dispatch events asynchronously
+    //     $baseEmitter  = new EventEmitter(); // this is basic, synchronous EventEmitter
+    //     $asyncEmitter = new EventEmitter($loop); // this EventEmitter uses loop to dispatch events asynchronously
 
-        $baseEmitter->on('event', function () {
-            echo "> \$baseEmitter received event!\n";
-        });
-        $asyncEmitter->on('event', function () {
-            echo "> \$asyncEmitter received event!\n";
-        });
+    //     $baseEmitter->on('event', function () {
+    //         echo "> \$baseEmitter received event!\n";
+    //     });
+    //     $asyncEmitter->on('event', function () {
+    //         echo "> \$asyncEmitter received event!\n";
+    //     });
 
-        echo "\nThis event is sync so it will\n";
-        $baseEmitter->emit('event');
-        echo "resolve listeners in the middle of printing this string\n";
+    //     echo "\nThis event is sync so it will\n";
+    //     $baseEmitter->emit('event');
+    //     echo "resolve listeners in the middle of printing this string\n";
 
-        echo "\nThis event is async so it will\n";
-        $asyncEmitter->emit('event');
-        echo "resolve listeners in the next cycle of loop, after string has been printed\n";
+    //     echo "\nThis event is async so it will\n";
+    //     $asyncEmitter->emit('event');
+    //     echo "resolve listeners in the next cycle of loop, after string has been printed\n";
 
-        $loop->onAfterTick(function () use ($loop) {
-            echo "\n";
-            $loop->stop();
-        });
-        $loop->start();
+    //     $loop->onAfterTick(function () use ($loop) {
+    //         echo "\n";
+    //         $loop->stop();
+    //     });
+    //     $loop->start();
 
-        $this->assertTrue(true);
-    }
+    //     $this->assertTrue(true);
+    // }
 }
