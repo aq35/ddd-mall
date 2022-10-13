e8b72ea885237a4572f21c53e28ec02c27c80589
kazuma hosobe <32251518+aq35@users.noreply.github.com>
2022-10-14 08:15:52 +0900

    不要ソースの削除

--- backend/DesignPattern/Queue/QueueAwareInterface.php
+++ backend/DesignPattern/Queue/QueueAwareInterface.php
@@ -2,6 +2,19 @@
 
 namespace DesignPattern\Queue;
 
-interface QueueAwareInterface extends QueueSetterAwareInterface, QueueGetterAwareInterface
+interface QueueAwareInterface
 {
+    /**
+     * Return the Queue of which object is aware of or null if none was set.
+     *
+     * @return QueueInterface|null
+     */
+    public function getQueue();
+
+    /**
+     * Set the Queue of which object is aware of or delete is setting to null.
+     *
+     * @param QueueInterface $Queue
+     */
+    public function setQueue(QueueInterface $Queue = null);
 }
