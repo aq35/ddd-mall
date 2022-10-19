527628c7bb78a1e930824be978c848db4ef4cb29
kazuma hosobe <32251518+aq35@users.noreply.github.com>
2022-10-20 07:02:48 +0900

    途中

--- backend/DesignPattern/Tests/OrderAsyncTest.php
+++ backend/DesignPattern/Tests/OrderAsyncTest.php
@@ -9,43 +9,63 @@ use DesignPattern\Event\ForClient\AsyncEventEmitter;
 // ./vendor/bin/phpunit DesignPattern/Tests/OrderAsyncTest.php
 final class OrderAsyncTest extends TestCase
 {
-    public function testEventEmitter_async_emitters(): void
+    public function test_EventEmitter_async_emitters(): void
     {
+        $asyncEmitter = new AsyncEventEmitter();
+        try {
+            $this->eventDriven($asyncEmitter);
+            $asyncEmitter->getQueue()->start();
+            echo "aa" . "\n";
+        } catch (\Exception $e) {
+            echo "失敗" . "\n";
+        }
+        $asyncEmitter->getQueue()->stop();
+        echo "テスト" . "\n";
+        $this->assertTrue(true);
+    }
 
+    public function eventDriven(AsyncEventEmitter $asyncEmitter): void
+    {
         $i = 0;
         echo "\n";
         echo "非同期処理";
         echo "\n";
-        $asyncEmitter = new AsyncEventEmitter();
 
         $i += 1;
-        $asyncEmitter->getQueue()->onStart(function () use ($i) {
-            echo "処理を開始します。" . "\n";
+        $asyncEmitter->getQueue()->onStart(function () use ($asyncEmitter, $i) {
+            echo "\e[31m [$i 番目] onStart \e[m" . "\n";
         });
-
         // -------------------------------------------------
+        echo "\n";
+        echo "非同期処理";
+        echo "\n";
         $i += 1;
-        $asyncEmitter->on('step1', function () use ($i) {
-            echo "step1" . "\n";
-            echo "注文の作成" . "\n";
+        $asyncEmitter->on('event', function () use ($i) {
+            echo "\e[31m [$i 番目] 'event' AsyncEventEmitter \e[m" . "\n";
         });
+        echo "\n";
+        echo "非同期処理";
+        echo "\n";
         $i += 1;
-        $asyncEmitter->on('step2', function () use ($asyncEmitter, $i) {
-            echo "顧客にメール通知" . "\n";
-            echo "決済の適用" . "\n";
-            echo "在庫引当" . "\n";
-            $asyncEmitter->emit('step3');
+        $asyncEmitter->on('event', function () use ($i, $asyncEmitter) {
+            echo "\e[31m [$i 番目] 'event' AsyncEventEmitter \e[m" . "\n";
         });
+        echo "\n";
+        echo "非同期処理";
+        echo "\n";
+        $asyncEmitter->emit('event');
 
-        $asyncEmitter->eventListenerForDebug();
-        // -------------------------------------------------
-
+        $i += 1;
+        $asyncEmitter->getQueue()->onAfterTick(function () use ($asyncEmitter, $i) {
+            echo "\e[31m [$i 番目] onAfterTick \e[m" . "\n";
+            $asyncEmitter->getQueue()->stop();
+        });
+        echo "\n";
+        echo "非同期処理";
+        echo "\n";
         $i += 1;
         $asyncEmitter->getQueue()->onStop(function () use ($asyncEmitter, $i) {
             echo "\e[31m [$i 番目] onStop \e[m" . "\n";
         });
-        $asyncEmitter->emit('step1');
-        $asyncEmitter->getQueue()->start();
-        $this->assertTrue(true);
     }
 }
